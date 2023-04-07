97c76db679d035243c8d6a893eecd6c16cd5910d
saras35 <74248101+saras35@users.noreply.github.com>
2023-04-07 21:03:45 +0530

    Add files via upload

---
+++ LOC-GROUP-17-/Week 02/w3_q1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+ 
+int main()
+{
+    char s[100],c,temp=1;  
+    int  i,j,k=0,n;
+ 
+    printf("Enter  the string : ");
+    gets(s);
+    
+	printf("Enter character: ");
+    c=getchar();
+
+    for(i=0;s[i];i++)
+    {
+     	s[i]=s[i+k];
+     	if(s[i]==c)
+     	{
+		  k++;
+		  i--;
+	    }
+   	}
+ 	 printf("%s",s);
+  return 0;
+}
---
+++ LOC-GROUP-17-/Week 02/w3_q3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int f, s, *p, *q, sum;
+    
+    printf("Enter the first number\n");
+    scanf("%d", &f);
+    printf("Enter the second number\n");
+    scanf("%d",&s);
+    
+    p = &f;
+    q = &s;
+    sum = *p + *q;
+    
+    printf("Sum of entered numbers = %d\n",sum);
+    
+    return 0;
+}
